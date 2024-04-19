653fc6118c4fa096b6b05de08a95f84282da3c17
gitchan <eunkeeeee@gmail.com>
2024-04-19 18:04:41 +0900

    feat: overload == operator

--- midterm/Examples/Ch8_6.cpp
+++ midterm/Examples/Ch8_6.cpp
@@ -21,7 +21,7 @@ private:
     int cent;
 };
 
-const Money operator+(const Money &amount1, const Money &amount2) {
+Money operator+(const Money &amount1, const Money &amount2) {
     int cents1 = amount1.getCents() + amount1.getDollars() * 100;
     int cents2 = amount2.getCents() + amount2.getDollars() * 100;
     int allCents = cents1 + cents2;
@@ -37,9 +37,23 @@ const Money operator+(const Money &amount1, const Money &amount2) {
     return Money(finalDollars, finalCents);
 }
 
+bool operator==(const Money &amount1, const Money &amount2) {
+    int cents1 = amount1.getCents() + amount1.getDollars() * 100;
+    int cents2 = amount2.getCents() + amount2.getDollars() * 100;
+
+    if (cents1 == cents2) {
+        return 1;
+    }
+    return 0;
+}
+
+
 int main() {
     Money money1(4, 2), money2(5, 2);
     Money add = money1 + money2;
 
     cout << add.getDollars() << " dollars " << add.getCents() << " cents";
+
+    Money m1(10, 99), m2(23, 57), m3(12, 34);
+    (m1 + m2) = m3;
 }
